9a78f3dc39b08c5d74c50afc6cbb095ff9413c44
Marcos Corasaniti <marcoscorasaniti@gmail.com>
2023-10-26 16:56:32 -0300

    Creacion de la rama/tarea para la creacion de la nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ItemViewComponent } from './components/item-view/item-view.component';
+import { NewRecipeComponent } from './components/new-recipe/new-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    SignUpComponent,
+    ItemViewComponent,
+    NewRecipeComponent
   ],
   imports: [
     BrowserModule,
