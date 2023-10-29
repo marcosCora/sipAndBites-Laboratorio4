9c601a201e1a6a53fd90e50486b18493d9a8939e
AngieHiga <angeles.higa@gmail.com>
2023-10-29 18:05:54 -0300

    modificacion de User

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,10 +1,6 @@
 export class User {
     
-<<<<<<< HEAD
     idUser : number = 0;
-=======
-    idUser : number = 0; 
->>>>>>> 9a9e83ba6d7e22dbdd8bd3a71530ff5575f63391
     firstName : string = '';
     lastName : string = '';
     dateOfBirth : Date = new Date();
