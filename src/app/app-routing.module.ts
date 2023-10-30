4678d6e79fb5fb6773746b4f496b32d2a5e299fe
AngieHiga <angeles.higa@gmail.com>
2023-10-29 23:14:30 -0300

    modificacion de componente login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewUserComponent } from './components/new-user/new-user.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'home', component: NewUserComponent},
-  {path:'newUser', component: NewUserComponent}
+  {path:'newUser', component: NewUserComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  user !: User;
+  usersList : User[] = [];
+  email : string = '';
+  password: string = '';
+  logInForm !: FormGroup; 
+  wrongLogIn : boolean = false;
+
+  constructor(private userService : UserService){}
+
+  ngOnInit(): void {
+
+    this.getAllUsers();
+
+    this.logInForm = new FormGroup({
+      'email' : new FormControl(this.email, [Validators.required]),
+      'password' : new FormControl(this.password, [Validators.required])
+    });
+  }
+
+  getAllUsers() : void {
+    this.userService.getUsers().subscribe((data : User[]) => {
+      this.usersList = data;
+      console.log(this.usersList);
+    });
+  }
+
+  logIn(){
+    
+    if(this.logInForm.invalid){
+      return;
+    }
+    
+    this.email = this.logInForm.controls['email'].value;
+    this.password = this.logInForm.controls['password'].value;
+    
+    if(this.validateLogIn()){
+      console.log("El mail existe");
+      console.log(this.email);
+      console.log(this.password);
+    }
+  }
+
+  validateLogIn() : boolean {
+
+    let exists : boolean = false;
+
+    for(let i=0 ; i < this.usersList.length ; i++ ){
+
+      if(this.email == this.usersList[i].email && this.password == this.usersList[i].password){
+        exists = true;
+        break;
+      }
+    }
+
+    if(!exists){
+      this.wrongLogIn = true;
+    }
+
+    return exists;
+  }
+
+  
 
 }
