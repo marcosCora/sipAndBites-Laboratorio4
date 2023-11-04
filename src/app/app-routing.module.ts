87c802ed7d613fd939c8e76f3ac2d4d47321572d
AngieHiga <angeles.higa@gmail.com>
2023-11-04 20:56:38 -0300

    add validations to user forms, add active attribute to user class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { NewUserComponent } from './components/new-user/new-user.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'home', component: NewUserComponent},
+  {path:'home', component: HomeComponent},
   {path:'newUser', component: NewUserComponent},
   {path: 'login', component: LoginComponent},
   {path: 'editUser', component: EditUserComponent}
