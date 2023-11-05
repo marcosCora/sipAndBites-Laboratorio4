f1d34487f6798c033454954d8770ff2099b7d90e
AngieHiga <angeles.higa@gmail.com>
2023-11-05 12:38:36 -0300

    user class modified and creation of NewMealComponent

---
+++ src/app/components/new-meal/new-meal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-meal',
+  templateUrl: './new-meal.component.html',
+  styleUrls: ['./new-meal.component.css']
+})
+export class NewMealComponent {
+
+}
